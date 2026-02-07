2f948235c2f62649ab61ffe7aa0a255dbb33edd4
TanK <theflashtank21@gmail.com>
2026-02-07 03:23:58 -0500

    feat: added FastAPI backend to generate tokens for Spotify Authorization     - Related issue: https://github.com/primetimetank21/apple-music-playlist-converter/issues/19

---
+++ src/backend/app.py
@@ -0,0 +1,56 @@
+# import os
+from typing import Final
+
+import httpx
+from fastapi import FastAPI, Query
+from fastapi.responses import RedirectResponse
+from pydantic import BaseModel
+
+from ..config import settings
+
+app = FastAPI()
+
+TOKEN_URL: Final[str] = "https://accounts.spotify.com/api/token"
+
+
+class TokenResponse(BaseModel):
+    access_token: str
+    refresh_token: str
+    expires_in: int
+
+
+@app.get("/login")
+def login():
+    """Redirect user to Spotify's login page."""
+    auth_url: str = (
+        "https://accounts.spotify.com/authorize?"
+        f"response_type=code&client_id={settings.CLIENT_ID}&"
+        f"scope={' '.join(settings.SCOPE)}&redirect_uri={settings.REDIRECT_URI}"
+    )
+    return RedirectResponse(auth_url)
+
+
+# TODO: Create frontend to receive the access token and refresh token
+@app.get("/callback", response_model=TokenResponse)
+def callback(code: str = Query(None)):
+    """Swap the code for an Access Token and Refresh Token."""
+    if not code:
+        return {"error": "Authorization failed"}
+
+    # Payload to exchange code for tokens
+    payload: dict[str, str] = {
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": settings.REDIRECT_URI,
+        "client_id": settings.CLIENT_ID,
+        "client_secret": settings.CLIENT_SECRET,
+    }
+
+    response = httpx.post(TOKEN_URL, data=payload)
+    token_response = response.json()
+
+    token_data = TokenResponse(**token_response)
+
+    # ACCESS_TOKEN: Used to make API calls (expires in 1hr)
+    # REFRESH_TOKEN: Used to get a new access token later
+    return token_data
